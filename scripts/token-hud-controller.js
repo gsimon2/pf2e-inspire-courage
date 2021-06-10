ddc9bb41bc0fe2ab3d9e33ad43b502f1ea14f734
Glen Simon <glen.a.simon@gmail.com>
2021-06-10 08:22:16 -0400

    Make compatitable with Foundry 0.8.x

--- scripts/token-hud-controller.js
+++ scripts/token-hud-controller.js
@@ -1,19 +1,17 @@
 const tokenButtonHandler = async (event, actor, token) => {
     const btn = $(event.currentTarget);
-    const inspireCourageItem = await getInspireCourageItem();
-    const activeToken = actor.getActiveTokens().find(t => t.data._id === token._id);
-    const inspireCourageEffects = getInspireCourageEffects(actor);
-
-    activeToken.toggleEffect('systems/pf2e/icons/spells/inspire-courage.jpg');
+    const inspireCourageItem = await getInspireCourageEffect();
+    const inspireCourageEffects = getInspireCourageEffectsFromActor(actor);
 
     if (inspireCourageEffects.length) {
         btn.removeClass('active');
-        actor.deleteEmbeddedEntity('OwnedItem', inspireCourageEffects.map(i => i._id));
+        actor.deleteEmbeddedDocuments('Item', inspireCourageEffects.map(i => i.id));
     } else {
         btn.addClass('active');
-        const effect = await actor.createOwnedItem(inspireCourageItem);
-        const update = {_id: effect._id, data: { expired: false, duration: {unit: 'unlimited'}}};
-        actor.updateEmbeddedEntity('OwnedItem', update);
+        const effects = await actor.createEmbeddedDocuments('Item', [inspireCourageItem.data]);
+        const inspireCourageEffect = effects.find(e => e.data.name === inspireCourageItem.data.name);
+        const update = {_id: inspireCourageEffect._id, data: {expired: false, duration: {unit: 'unlimited'}}};
+        actor.updateEmbeddedDocuments('Item', [update]);
     }
 };
 
@@ -21,7 +19,7 @@ const createButton = (actor) => {
     let btn = document.createElement('div');
 
     btn.classList.add('control-icon');
-    getInspireCourageEffects(actor).length ? btn.classList.add('active') : {};
+    getInspireCourageEffectsFromActor(actor).length ? btn.classList.add('active') : {};
     btn.innerHTML = `<i class="fas fa-music fa-fw"></i>`
     btn.title = game.i18n.localize('pf2InspireCourage.ToolTipText');
 
@@ -47,13 +45,13 @@ export const prepTokenHUD = (hud, html, token) => {
     }
 };
 
-const getInspireCourageItem = async () => {
+const getInspireCourageEffect = async () => {
     const spellEffectPack = game.packs.get('pf2e.spell-effects');
     await spellEffectPack.getIndex(); // Have to force the compendium to load content
     const InspireCourageEntry = spellEffectPack.index.find(e => e.name.includes('Inspire Courage'));
-    return await spellEffectPack.getEntity(InspireCourageEntry._id);
+    return await spellEffectPack.getDocument(InspireCourageEntry._id);
 };
 
-const getInspireCourageEffects = (actor) => {
+const getInspireCourageEffectsFromActor = (actor) => {
     return (actor.data).items.filter(i => i.name.toLowerCase().includes('inspire courage') && i.type === 'effect');
 };
